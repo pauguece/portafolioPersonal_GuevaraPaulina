6a3c3794049482b0f3ff05ede49d846cbc3cfeab
pauguece <paulinamichelgc@gmail.com>
2026-09-05 13:22:27 -0700

    Primer commit

---
+++ src/test/java/com/example/MiPortafolioPersonal/MiPortafolioPersonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MiPortafolioPersonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiPortafolioPersonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
